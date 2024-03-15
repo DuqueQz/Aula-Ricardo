3ed6314353b285a7acebaab54887642f9f672130
DuqueQz <matheustogni64@gmail.comm>
2024-03-14 21:42:27 -0300

    att

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const port = 3000;
 app.use(express.json());
 require('./services/swagger')
 
+require('./routes')(app)
 app.get('/', (req,res) => {res.send('Hello World!'); });
 
-require('./routes')(app)
+app.use('/v1/docs', express.static('src/views'))
+app.use('/docs/swagger.yaml', express.static('src/docs/swagger.yaml'))
 
 app.listen(port, ()=> {
     console.log(`Aplicação rodando na porta ${port}`);
--- src/routes/Aluno.js
+++ src/routes/Aluno.js
@@ -1,9 +1,21 @@
 const alunoController = require('../controllers/aluno');
 
 module.exports = (app) =>{
-    app.get('/aluno', alunoController.getAluno)
-    app.post('/aluno', alunoController.postAluno)
-    app.delete('/aluno/:id', alunoController.deleteAluno)
-    app.put('/aluno/:id', alunoController.putAluno)
-    app.patch('/aluno/:id', alunoController.patchAluno)
+    app.get('/aluno', alunoController.getAluno
+    // #swagger.tags = ["aluno"]
+    // #swagger.summary = 'Consulta a lista de alunos'
+    // #swagger.description = 'Consulta lista de alunos cadastrados' 
+    )
+    app.post('/aluno', alunoController.postAluno
+    // #swagger.tags = ["aluno"]
+    )
+    app.delete('/aluno/:id', alunoController.deleteAluno
+    // #swagger.tags = ["aluno"]
+    )
+    app.put('/aluno/:id', alunoController.putAluno
+    // #swagger.tags = ["aluno"]
+    )
+    app.patch('/aluno/:id', alunoController.patchAluno
+    // #swagger.tags = ["aluno"]
+    )
 }
