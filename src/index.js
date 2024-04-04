9c66deb866278ef3cadf8778fc48021efb37dd2e
Matheus Togni <matheustogni64@gmail.com>
2024-04-04 19:08:09 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cookieParser());
 require('./services/swagger')
 
 require('./routes')(app)
-app.get('/', (req,res) => {res.send('Hello World!'); });
+app.get('/', (req,res) => {
+    res.cookie('meuCookie2024', 'abcde');
+    res.send('Hello World!'); });
 
 app.use('/v1/docs', express.static('src/views'))
 app.use('/docs/swagger.yaml', express.static('src/docs/swagger.yaml'))
@@ -14,3 +18,5 @@ app.use('/docs/swagger.yaml', express.static('src/docs/swagger.yaml'))
 app.listen(port, ()=> {
     console.log(`Aplicação rodando na porta ${port}`);
 });
+
+
